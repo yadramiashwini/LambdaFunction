44fa7993a3e587e557d37b1880f29eaa1d759cc6
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-09 22:04:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LambdaFunction
+{
+
+   
+        public class lambdaFunction
+        {
+            public delegate string PrintDelegate(string name);     //declaring delegate function
+
+            static void Main(string[] args)
+            {
+
+            PrintDelegate p = name => { return name; };              //lamdba expression object using delegate function
+            
+                string str = p("Ashwini");
+                Console.WriteLine("name is:" + str);
+
+            }
+        }
+    }
+
